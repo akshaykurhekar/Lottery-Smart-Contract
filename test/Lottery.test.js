c3b24b609f733494f546e64ae70c78925600008b
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-12-07 12:41:53 +0530

    initial commit

---
+++ test/Lottery.test.js
@@ -0,0 +1,91 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
+let lottery;
+let accounts;
+
+// beforeEach will execute each time before every test case, so we define account and instance of contract in it.
+beforeEach( async ()=>{
+    accounts = await web3.eth.getAccounts();
+
+    // instance of contract
+    lottery = await new web3.eth.Contract( JSON.parse(interface))
+    .deploy({ data:bytecode })
+    .send({ from:accounts[0], gas:'1000000'});
+});
+
+
+describe('Lottery test',()=>{
+    it('Deployed Contract', ()=>{
+        assert.ok(lottery.options.address);
+    });
+
+    it('allow one account to enter',async ()=>{
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({ from: accounts[0]});
+
+        // check player array has exact address
+        assert.equal(accounts[0], players[0]);
+
+        // check player has length 1
+        assert.equal(1, players.length);
+
+    });
+
+    it('allow multiple account to enter',async ()=>{
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({ from: accounts[0]});
+
+        // check player array has exact address
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+
+        // check player has length 1
+        assert.equal(3, players.length);
+
+    });
+
+    it('requires a minimum amount to enter',async ()=>{
+        try{
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 0    // this value in wei
+            });
+            throw(false);  // it will fail test if error not occurs 
+        }catch(err){
+            assert.ok(err);  // it will check err is exist or not;
+        }
+    });
+
+    it('only manager can pickWinner',async ()=>{
+        
+        await lottery.methods.pickWinner().send({
+            from: accounts[1],
+        });
+        
+          
+    });
+});
